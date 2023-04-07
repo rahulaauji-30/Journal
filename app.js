a130b5ceb98919f82c9a06d2deed402061ee9624
Rahul Parihar <rahulaauji71@gmail.com>
2023-04-07 14:24:20 +0530

    all the files

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+require("dotenv").config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+
+const intro = "Hit the  + icon to add new notes";
+const aboutContent = "The web App is devleoped as a college project not for the actual use now .It is developed by T=the students of first year BSC-CS Viswakarma University Kondhwa Pune. For more infromation contact rahulaauji71@gmail.coms";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect(process.env.URL, {useNewUrlParser: true});
+console.log(process.env.URL);
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: intro,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
